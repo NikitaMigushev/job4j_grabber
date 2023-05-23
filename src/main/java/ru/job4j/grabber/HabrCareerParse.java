22eb547d60403b139e73e16062793cf697a7d2fc
Nikita Migushev <migushev@gmail.com>
2023-05-23 16:44:38 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -12,23 +12,27 @@ import java.time.format.DateTimeFormatter;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final String PAGE_LINK_FORMAT = "%s/vacancies/java_developer?page=%d";
+    private static final int NUM_PAGES = 5;
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
         DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            Element dateTimeElement = row.select(".vacancy-card__date").first();
-            String dateTime = dateTimeElement.select("time").attr("datetime");
-            LocalDateTime parsedDateTime = dateTimeParser.parse(dateTime);
-            String formattedDateTime = parsedDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-            System.out.printf("%s %s %s%n", vacancyName, link, formattedDateTime);
-        });
+        for (int page = 1; page <= NUM_PAGES; page++) {
+            String pageLink = String.format(PAGE_LINK_FORMAT, SOURCE_LINK, page);
+            Connection connection = Jsoup.connect(pageLink);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                Element dateTimeElement = row.select(".vacancy-card__date").first();
+                String dateTime = dateTimeElement.select("time").attr("datetime");
+                LocalDateTime parsedDateTime = dateTimeParser.parse(dateTime);
+                String formattedDateTime = parsedDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                System.out.printf("%s %s %s%n", vacancyName, link, formattedDateTime);
+            });
+        }
     }
 }
