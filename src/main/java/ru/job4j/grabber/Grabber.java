910e14a643fb038cd02fb5922badf790dd19927a
Nikita Migushev <migushev@gmail.com>
2023-05-24 07:45:51 +0300

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -2,10 +2,11 @@ package ru.job4j.grabber;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.quartz.AlertRabbit;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.Properties;
 
@@ -18,6 +19,7 @@ public class Grabber implements Grab {
     private final Store store;
     private final Scheduler scheduler;
     private final int time;
+    private final Properties config;
 
     private static final String PAGE_LINK_FORMAT = "%s/vacancies/java_developer?page=%d";
 
@@ -26,6 +28,7 @@ public class Grabber implements Grab {
         this.store = store;
         this.scheduler = scheduler;
         this.time = time;
+        this.config = readProperties();
     }
 
     @Override
@@ -63,6 +66,46 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(config.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    OutputStream out = socket.getOutputStream();
+                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(out, "windows-1251"), true);
+                    StringBuilder htmlBuilder = new StringBuilder();
+                    htmlBuilder.append("<html><body>");
+                    htmlBuilder.append("<h1>List Content</h1>");
+                    for (Post post : store.getAll()) {
+                        htmlBuilder.append("<div>");
+                        htmlBuilder.append(String.format("<h2> <a href=\"%s\">", post.getLink())).append(post.getTitle()).append("</a> </h2>");
+                        htmlBuilder.append("<p>").append(post.getDescription()).append("</p>");
+                        htmlBuilder.append("</div>");
+                    }
+                    htmlBuilder.append("</body></html>");
+                    String htmlResponse = htmlBuilder.toString();
+                    writer.println("HTTP/1.1 200 OK");
+                    writer.println("Content-Type: text/html; charset=windows-1251");
+                    writer.println("Content-Length: " + htmlResponse.getBytes("windows-1251").length);
+                    writer.println();
+                    writer.println(htmlResponse);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private static Properties readProperties() {
+        Properties properties = new Properties();
+        try (InputStream in = new FileInputStream("db/liquibase.properties")) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
     public static void main(String[] args) throws Exception {
         var cfg = new Properties();
         try (InputStream in = new FileInputStream("db/liquibase.properties")) {
@@ -75,7 +118,6 @@ public class Grabber implements Grab {
         var time = Integer.parseInt(cfg.getProperty("time"));
         Grabber grabber = new Grabber(parse, store, scheduler, time);
         grabber.init();
-        Thread.sleep(3000000);
-        scheduler.shutdown(true);
+        grabber.web(new PsqlStore(new Properties()));
     }
 }
